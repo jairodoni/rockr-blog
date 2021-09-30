222a38e68b46c6997ea1ae13af50685c99bae3c8
jairodoni <jairo.doni97@gmail.com>
2021-09-30 20:22:55 -0300

    Contact Form receiving data

--- src/components/ContactModal/index.tsx
+++ src/components/ContactModal/index.tsx
@@ -1,14 +1,26 @@
 import Modal from 'react-modal';
+import { useForm } from 'react-hook-form';
+
 import styles from './styles.module.scss'
 
+interface Message {
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+}
 interface ContactModalProps {
   isOpenContact: boolean;
   onRequestClose: () => void;
 }
 
 export function ContactModal({ isOpenContact, onRequestClose }: ContactModalProps) {
+  const { register, handleSubmit, reset } = useForm();
 
-  async function handleSubmitFormContact() {
+
+  async function handleSubmitFormContact(data: Message) {
+    reset();
+    onRequestClose()
     return;
   }
 
@@ -27,16 +39,31 @@ export function ContactModal({ isOpenContact, onRequestClose }: ContactModalProp
       >
         <img src="/images/close.svg" alt="Close modal" />
       </button>
-      <form onSubmit={handleSubmitFormContact} className={styles.container}>
+      <form onSubmit={handleSubmit(handleSubmitFormContact)} className={styles.container}>
         <h2>Contact</h2>
         <label>Name</label>
-        <input type="text" placeholder="Fill your full name" />
+        <input
+          type="text"
+          placeholder="Fill your full name"
+          {...register('name')}
+        />
         <label>E-mail</label>
-        <input type="text" placeholder="Fill a valid e-mail" />
+        <input
+          type="email"
+          placeholder="Fill a valid e-mail"
+          {...register('email')}
+        />
         <label>Phone</label>
-        <input type="text" placeholder="Fill your phone" />
+        <input
+          type="phone"
+          placeholder="Fill your phone"
+          {...register('phone')}
+        />
         <label>Post</label>
-        <textarea placeholder="Hello..." />
+        <textarea
+          placeholder="Hello..."
+          {...register('message')}
+        />
 
         <div className={styles.submit}>
           <button type="submit">
